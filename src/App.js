4c3a43fa66de992c904ab77cdf8d31f947120b8a
MellowMokoen <mamello3632@gmail.com>
2023-12-07 19:59:34 +0200

    Styled the MenuSidebar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from "./pages/Homepage";
 import ItemDetailsDisplay from './components/ItemDetailsDisplay';
 import BagReview from './components/BagReview';
+import BagItems from "./components/BagItems";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Homepage />} />
           <Route path="/product-details" element={<ItemDetailsDisplay />} />
+          <Route path='/product-details' element={<BagItems />} />
           <Route path='/cart' element={<BagReview />} />
         </Routes>
       </Router>
