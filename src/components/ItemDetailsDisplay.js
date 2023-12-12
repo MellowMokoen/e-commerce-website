3db5180469a7380fcbed4674b7ab61dc679f7858
MellowMokoen <mamello3632@gmail.com>
2023-12-12 14:40:12 +0200

    Used Redux for the cart display

--- src/components/ItemDetailsDisplay.js
+++ src/components/ItemDetailsDisplay.js
@@ -1,3 +1,115 @@
+// ProductItems.js
+import React from "react";
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../cartSlice';
+import * as images from "./images";
+import { Link } from "react-router-dom";
+
+const itemsData = [
+  {
+    id: 1,
+    name: "Watch",
+    image: images.product1,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    rating: 4.5,
+    price: 199.99,
+  },
+
+  {
+    id: 2,
+    name: "Headphones",
+    image: images.product2,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+
+  {
+    id: 3,
+    name: "Iphone 11- Series Black",
+    image: images.product3,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+
+  {
+    id: 4,
+    name: "Iphone 11- Series Blue",
+    image: images.product4,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+
+  {
+    id: 5,
+    name: "Iphone 11- Product Red",
+    image: images.product5,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+
+  {
+    id: 6,
+    name: "Iphone 11- Milky White",
+    image: images.product6,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+
+  {
+    id: 7,
+    name: "Iphone 13- Product Red",
+    image: images.product7,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+
+  {
+    id: 8,
+    name: "Iphone 14- Product Red",
+    image: images.product8,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+]; // Your item data array
+
+const ProductItems = () => {
+  const dispatch = useDispatch();
+
+  const onAddToCart = (item) => {
+    dispatch(addToCart({ ...item, quantity: 1 }));
+  };
+
+  return (
+    <div className="product-container">
+      <div className="row">
+        {itemsData.map((item) => (
+          <ItemDetailsDisplay
+            key={item.id}
+            name={item.name}
+            image={item.image}
+            description={item.description}
+            rating={item.rating}
+            price={item.price}
+            onAddToCart={() => onAddToCart(item)}
+          />
+        ))}
+      </div>
+      <Link to="/cart">
+        <button className="review-cart-button">Review Cart</button>
+      </Link>
+    </div>
+  );
+};
+
+const ItemDetailsDisplay = ({ name, image, description, rating, price, onAddToCart }) => (
+  <div className="item-card">
+    <h2>{name}</h2>
+    <img src={image} alt={name} className="item-image" />
+    <p>{description}</p>
+    <p>Rating: {rating}</p>
+    <p>Price: {price}</p>
+    <button onClick={onAddToCart}>Add to Cart</button>
+  </div>
+);
+
+export default ProductItems;
+
+
+
+/*
 import React, { useState } from "react";
 import * as images from "./images";
 import { Link } from "react-router-dom";
@@ -112,6 +224,6 @@ const ItemDetailsDisplay = ({ name, image, description, rating, price, onAddToCa
   </div>
 );
 
-
+*/
 
 
