0524687e362913ce40b53109a26d11ae37974eeb
MellowMokoen <mamello3632@gmail.com>
2024-01-27 18:47:05 +0200

    Updated the bagsidebar css import

--- src/components/BagSidebar.js
+++ src/components/BagSidebar.js
@@ -2,10 +2,9 @@ import React from "react";
 import "../styles/BagSidebar.css";
 
 export default function BagSidebar() {
-    return (
-        <div className="bag-container">
-            
-            <button className="bag-view">View Bag</button>
-        </div>
-    );
+  return (
+    <div className="bag-container">
+      <button className="bag-view">View Bag</button>
+    </div>
+  );
 }
