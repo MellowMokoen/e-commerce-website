18b41f7766e2156f30b5cec27950fa5477f3c76b
MellowMokoen <mamello3632@gmail.com>
2024-01-28 23:59:18 +0200

    Added more items to the data

--- src/components/images.js
+++ src/components/images.js
@@ -6,6 +6,16 @@ import product5 from "../images/Name=Iphone-12-03.png";
 import product6 from "../images/Name=Iphone-12-04.png";
 import product7 from "../images/Name=Iphone-12-Pro-01.png";
 import product8 from "../images/Name=Iphone-12-Pro-02.png";
+import product9 from "../images/Name=Dell-XPS-13-Black.png";
+import product10 from "../images/Name=Dell-XPS-13-White.png";
+import product11 from "../images/Name=Dell-XPS-15-Black.png";
+import product12 from "../images/Name=Macbook.png";
+import product13 from "../images/Name=Samsung-Note21.png";
+import product14 from "../images/Name=Samsung-S21-Pro.png";
+import product15 from "../images/Name=Samsung-S21.png";
+import product16 from "../images/Name=Iphone-13-Pro-01.png";
+import product17 from "../images/Name=Iphone-13-Pro-02.png";
+import product18 from "../images/Name=Iphone-13-Pro-03.png";
 
 export {
   product1,
@@ -16,4 +26,14 @@ export {
   product6,
   product7,
   product8,
+  product9,
+  product10,
+  product11,
+  product12,
+  product13,
+  product14,
+  product15,
+  product16,
+  product17,
+  product18,
 };
